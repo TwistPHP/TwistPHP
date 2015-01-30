eacccf6396915119e4a3e0418954d6748b134abd
Dan Walker <danrwalker1@gmail.com>
2015-01-30 11:13:08 +0000

    Massive upgrade to functionality and support
    Added in PATCH, DELETE and allot of new functionality

--- src/twist/core/packages/Curl.twist.php
+++ src/twist/core/packages/Curl.twist.php
@@ -31,7 +31,10 @@
 
 		protected $blResponseJSON = false;
 		protected $blDisableUrlEncoding = false;
+		protected $blVerifySSLRequest = false;
+		protected $arrSSLCertificate = array();
 		protected $strDefaultUserAgent = 'TwistPHP Curl';
+		protected $strCookies = '';
 		protected $strUserAgent = '';
 		protected $strUserPassword = null;
 		protected $arrRequestInfo = array();
@@ -58,6 +61,15 @@
 		 */
 		public function disableUrlEncoding($blEnable = true){
 			$this->blDisableUrlEncoding = $blEnable;
+		}
+
+		/**
+		 * Stop the system from URL encoding all parameters before they are sent
+		 *
+		 * @param $blEnable Determines if functionality should be used
+		 */
+		public function verifySSLRequest($blEnable = true){
+			$this->blVerifySSLRequest = $blEnable;
 		}
 
 		/**
@@ -89,6 +101,41 @@
 		}
 
 		/**
+		 * Encrypt the Curl request using a SSL certificate and key pair
+		 *
+		 * @param $dirSSLCertificate The path of a file containing a PEM formatted certificate.
+		 * @param $dirSSLKey The path of a file containing a private SSL key.
+		 * @param $strCertificateType The format of the certificate. Supported formats are "PEM" (default), "DER", and "ENG".
+		 * @param $strKeyType The key type of the private SSL key. Supported key types are "PEM" (default), "DER", and "ENG".
+		 */
+		public function setSSLCertificate($dirSSLCertificate,$dirSSLKey,$strCertificateType = 'PEM',$strKeyType = 'PEM'){
+			$this->arrSSLCertificate = array(
+				'certificate' => $dirSSLCertificate,
+				'key' => $dirSSLKey,
+				'certificate_type' => $strCertificateType,
+				'key_type' => $strKeyType
+			);
+		}
+
+		/**
+		 * Set the content type of the request, this will be merged with the headers array if required
+		 *
+		 * @param $strContentType Content type of the body data (if required)
+		 */
+		public function setContentType($strContentType){
+			$this->arrHeaders[] = sprintf("Content-Type: %s",trim($strContentType));
+		}
+
+		/**
+		 * The contents of the "Cookie: " header to be used in the HTTP request. Note that multiple cookies are separated with a semicolon followed by a space (e.g., "fruit=apple; colour=red")
+		 *
+		 * @param $strCookies The cookie content to be, separated multiple cookies with a semicolon followed by a space
+		 */
+		public function setCookies($strCookies){
+			$this->strCookies = $strCookies;
+		}
+
+		/**
 		 * Make a GET request to the provided URL, set the User Agent header when required
 		 *
 		 * @param $strURL Full URL for the request
@@ -96,22 +143,23 @@
 		 * @param $arrHeaders Array of additional headers to be sent
 		 * @return mixed Returns the results of the request, will be an array if 'decodeResponseJSON' is enabled
 		 */
-		public function get($strURL,$arrRequestData,$arrHeaders = array()){
+		public function get($strURL,$arrRequestData = array(),$arrHeaders = array()){
 			return $this->makeRequest($strURL,$arrRequestData,'get',$arrHeaders);
 		}
 
 		/**
-		 * Make a POST request to the provided URL, set the User Agent header when required
+		 * Make a POST request to the provided URL, set the User Agent header when required. The post data can either be an array of parameters or raw body data e.g. XML/JSON.
+		 * If Raw data is used you must specify a content type for the request.
 		 *
 		 * @related get
 		 *
 		 * @param $strURL Full URL for the request
-		 * @param $arrData Array of post parameters
+		 * @param $mxdRequestData Pass in an array of post parameters or raw body data of e.g. XML,JSON to be posted
 		 * @param $arrHeaders Array of additional headers to be sent
 		 * @return mixed Returns the results of the request, will be an array if 'decodeResponseJSON' is enabled
 		 */
-		public function post($strURL,$arrData,$arrHeaders = array()){
-			return $this->makeRequest($strURL,$arrData,'post',$arrHeaders);
+		public function post($strURL,$mxdRequestData = array(),$arrHeaders = array()){
+			return $this->makePostRequest($strURL,$mxdRequestData,$arrHeaders,'delete');
 		}
 
 		/**
@@ -121,45 +169,85 @@
 		 *
 		 * @param $strURL Full URL for the request
 		 * @param $strRawData Raw data to be posted
-		 * @param $mxdRequestData Array of get parameters
+		 * @param $arrRequestData Array of get parameters
 		 * @param $arrHeaders Array of additional headers to be sent
 		 * @return mixed Returns the results of the request, will be an array if 'decodeResponseJSON' is enabled
 		 */
-		public function put($strURL,$strRawData,$mxdRequestData = array(),$arrHeaders = array()){
-			return $this->makeRequest($strURL,$mxdRequestData,'put',$arrHeaders,$strRawData);
+		public function put($strURL,$strRawData,$arrRequestData = array(),$arrHeaders = array()){
+			return $this->makeRequest($strURL,$arrRequestData,'put',$arrHeaders,$strRawData);
 		}
 
 		/**
-		 * Make a DELETE request to the provided URL, set the User Agent header when required
+		 * Make a PATCH request to the provided URL, set the User Agent header when required. The post data can either be an array of parameters or raw body data e.g. XML/JSON.
+		 * If Raw data is used you must specify a content type for the request.
 		 *
 		 * @related get
 		 *
 		 * @param $strURL Full URL for the request
-		 * @param $mxdRequestData Array of get parameters
+		 * @param $mxdRequestData Pass in an array of post parameters or raw body data of e.g. XML,JSON to be posted
 		 * @param $arrHeaders Array of additional headers to be sent
 		 * @return mixed Returns the results of the request, will be an array if 'decodeResponseJSON' is enabled
 		 */
-		public function delete($strURL,$mxdRequestData,$arrHeaders = array()){
-			return $this->makeRequest($strURL,$mxdRequestData,'delete',$arrHeaders);
+		public function patch($strURL,$mxdRequestData = array(),$arrHeaders = array()){
+			return $this->makePostRequest($strURL,$mxdRequestData,$arrHeaders,'delete');
+		}
+
+		/**
+		 * Make a DELETE request to the provided URL, set the User Agent header when required. The post data can either be an array of parameters or raw body data e.g. XML/JSON.
+		 * If Raw data is used you must specify a content type for the request.
+		 *
+		 * @related get
+		 *
+		 * @param $strURL Full URL for the request
+		 * @param $mxdRequestData Pass in an array of post parameters or raw body data of e.g. XML,JSON to be posted
+		 * @param $arrHeaders Array of additional headers to be sent
+		 * @return mixed Returns the results of the request, will be an array if 'decodeResponseJSON' is enabled
+		 */
+		public function delete($strURL,$mxdRequestData = array(),$arrHeaders = array()){
+			return $this->makePostRequest($strURL,$mxdRequestData,$arrHeaders,'delete');
+		}
+
+		/**
+		 * A method used but POST, PATCH and DELETE as they are all similar requests with a different method name
+		 *
+		 * @param $strURL Full URL for the request
+		 * @param $mxdRequestData Pass in an array of post parameters or raw body data of e.g. XML,JSON to be posted
+		 * @param $arrHeaders Array of additional headers to be sent
+		 * @return mixed Returns the results of the request, will be an array if 'decodeResponseJSON' is enabled
+		 */
+		protected function makePostRequest($strURL,$mxdRequestData = array(),$arrHeaders = array(),$strMethod = 'post'){
+
+			$arrRequestData = array();
+			$strRawData = '';
+
+			//Decide if the request is post fields or body data
+			if(is_array($mxdRequestData)){
+				$arrRequestData = $mxdRequestData;
+			}else{
+				$strRawData = $mxdRequestData;
+			}
+
+			return $this->makeRequest($strURL,$arrRequestData,$strMethod,$arrHeaders,$strRawData);
 		}
 
 		/**
 		 * The function that makes the CURL requests, all data is passed in and the response is returned
 		 *
 		 * @param $strURL Full URL for the request
-		 * @param $mxdRequestData Array of post/get parameters
+		 * @param $arrRequestData Array of post/get parameters
 		 * @param $strType HTTP protocol of the request
 		 * @param $arrHeaders Array of additional headers to be sent
 		 * @param $strRawData Raw data to be posted
 		 * @return mixed Returns the results of the request, will be an array if 'decodeResponseJSON' is enabled
 		 */
-		protected function makeRequest($strURL,$mxdRequestData,$strType = 'get',$arrHeaders=array(),$strRawData = ''){
-
-			$strData = "";
+		protected function makeRequest($strURL,$arrRequestData = array(),$strType = 'get',$arrHeaders=array(),$strRawData = ''){
+			
+			$strData = null;
 
 			//Set the all the parameters
-			if(is_array($mxdRequestData) && count($mxdRequestData)){
-				foreach($mxdRequestData as $mxdKey => $mxdData){
+			if(is_array($arrRequestData) && count($arrRequestData)){
+				$strData = '';
+				foreach($arrRequestData as $mxdKey => $mxdData){
 					$strParamData = ($this->blDisableUrlEncoding) ? $mxdData : urlencode($mxdData);
 					$strData .= sprintf("%s=%s&",$mxdKey,$strParamData);
 				}
@@ -168,25 +256,35 @@
 			//open connection
 			$resCurl = curl_init();
 
-			switch( $strType ) {
-				case 'get':
+			switch($strType){
 
-					if($strData != ''){
-						$strURL = sprintf("%s?%s",$strURL,$strData);
-					}
+				case 'get':
+					$strURL = (!is_null($strData)) ? sprintf("%s?%s",$strURL,$strData) : $strURL;
 					break;
 
+				case 'patch':
+				case 'delete':
 				case 'post':
-					//Set the post vars if a post request
-					if($strData != ''){
-						curl_setopt($resCurl, CURLOPT_POST, count($mxdRequestData)+1);
-						curl_setopt($resCurl, CURLOPT_POSTFIELDS, $strData);
+
+					//Set the custom request method name
+					if($strType != 'post'){
+						curl_setopt($resCurl, CURLOPT_CUSTOMREQUEST, strtoupper($strType));
 					}
+
+					//Set the post vars if a post request or Raw body data if required
+					if(!is_null($strData)){
+						curl_setopt($resCurl, CURLOPT_POST, count($arrRequestData)+1);
+						curl_setopt($resCurl, CURLOPT_POSTFIELDS, $strData);
+					}elseif($strRawData != ''){
+						curl_setopt($resCurl, CURLOPT_POST, 1);
+						curl_setopt($resCurl, CURLOPT_POSTFIELDS, $strRawData);
+					}
+
 					break;
 
 				case 'put':
 
-					if($strData != ''){
+					if(!is_null($strData)){
 						$strURL = sprintf("%s?%s",$strURL,$strData);
 					}
 
@@ -201,10 +299,6 @@
 					curl_setopt($resCurl, CURLOPT_INFILE, $resRequestBody);
 					curl_setopt($resCurl, CURLOPT_INFILESIZE, strlen($strRawData));
 					break;
-
-				case 'delete':
-					curl_setopt($resCurl, CURLOPT_CUSTOMREQUEST, 'DELETE');
-					break;
 			}
 
 			//set the url
@@ -215,9 +309,18 @@
 				curl_setopt($resCurl, CURLOPT_USERPWD, $this->strUserPassword);
 			}
 
+			//Set the SSL encryption certificate and key
+			if(count($this->arrSSLCertificate)){
+				curl_setopt($resCurl, CURLOPT_SSLCERT, $this->arrSSLCertificate['certificate']);
+				curl_setopt($resCurl, CURLOPT_SSLCERTTYPE, $this->arrSSLCertificate['certificate_type']);
+				curl_setopt($resCurl, CURLOPT_SSLKEY, $this->arrSSLCertificate['key']);
+				curl_setopt($resCurl, CURLOPT_SSLKEYTYPE, $this->arrSSLCertificate['key_type']);
+			}
+
+			curl_setopt($resCurl, CURLOPT_SSL_VERIFYHOST, ($this->blVerifySSLRequest) ? 1 : 0);
+			curl_setopt($resCurl, CURLOPT_SSL_VERIFYPEER, ($this->blVerifySSLRequest) ? 1 : 0);
+
 			//Set the other data
-			curl_setopt($resCurl, CURLOPT_SSL_VERIFYHOST, 0);
-			curl_setopt($resCurl, CURLOPT_SSL_VERIFYPEER, 0);
 			curl_setopt($resCurl, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($resCurl, CURLOPT_TIMEOUT, $this->intTimeout);
 			curl_setopt($resCurl, CURLOPT_FAILONERROR, true);
@@ -225,6 +328,11 @@
 			//Set the custom headers
 			if(is_array($arrHeaders) && count($arrHeaders) > 0){
 				curl_setopt($resCurl, CURLOPT_HTTPHEADER,$arrHeaders);
+			}
+
+			//Set some cookies for the request if required
+			if($this->strCookies != ''){
+				curl_setopt($resCurl, CURLOPT_COOKIE, $this->strCookies);
 			}
 
 			//Set the custom User Agent Header
