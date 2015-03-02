7193cbbf1b061b46d36bc658ae140a61b8d8e854
Dan Walker <danrwalker1@gmail.com>
2015-03-02 15:23:15 +0000

    Added default files to aid initial install of v3
    Folder structure to be comfirmed

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<?php
+
+	/* ================================================================================
+	 * TwistPHP - Default index.php
+	 * --------------------------------------------------------------------------------
+	 * Author:          Shadow Technologies Ltd.
+	 * Documentation:   https://twistphp.com/docs
+	 * ================================================================================
+	 */
+
+	require_once 'twist/framework.php';
+
+	//TWISTPHP Interfaces
+
+
+	//TWISTPHP Routes
+
+
+	//TWISTPHP Serve
