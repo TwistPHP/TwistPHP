87bbd16dd252604a0d251122f401335dea7ec0a5
Andrew Hosgood <andi@floor44.co.uk>
2015-05-06 14:16:58 +0100

    CSV reformatting

--- src/twist/core/packages/CSV.twist.php
+++ src/twist/core/packages/CSV.twist.php
@@ -22,30 +22,32 @@
 	 */
 
 	namespace Twist\Core\Packages;
+
 	use \Twist\Core\Classes\BasePackage;
 
 	/**
 	 * Simply Create, Serve and Import CSV files. Create a CSV file from and array of data, database query results can be directly exported as a CSV file with. Import CSV files into a usable indexed array of data.
 	 */
-	class CSV extends BasePackage{
+	class CSV extends BasePackage {
 
-		public function __construct(){}
+		public function __construct() {}
 
 		/**
 		 * Create a CSV file on the server, pass in a multi-dimensional array of data containing keys and values, the keys will be used as the field names and the values will be each for in the CSV. By default the Delimiter, Enclosure and Escape are already set.
 		 *
 		 * @param $strLocalFile Full path to the local CSV file to be stored
-		 * @param $arrData Multi-dimensional array of data to be converted into a CSV
+		 * @param $arrData      Multi-dimensional array of data to be converted into a CSV
 		 * @param $strDelimiter Delimiter to be used in creation of CSV data
 		 * @param $strEnclosure Enclosure to be used in creation of CSV data
+		 *
 		 * @return mixed Returns the CSV data as a string
 		 */
-		public function export($strLocalFile,$arrData,$strDelimiter = ',',$strEnclosure = '"'){
+		public function export( $strLocalFile, $arrData, $strDelimiter = ',', $strEnclosure = '"' ) {
 
-			$mxdOut = $this->generateCSV($arrData,$strDelimiter,$strEnclosure);
+			$mxdOut = $this->generateCSV( $arrData, $strDelimiter, $strEnclosure );
 
 			//Create the CSV file on the server
-			file_put_contents($strLocalFile,$mxdOut);
+			file_put_contents( $strLocalFile, $mxdOut );
 
 			return $mxdOut;
 		}
@@ -53,18 +55,18 @@
 		/**
 		 * Create a CSV file and serve to the user, pass in a multi-dimensional array of data containing keys and values, the keys will be used as the field names and the values will be each for in the CSV. By default the Delimiter, Enclosure and Escape are already set.
 		 *
-		 * @param $strFileName Name of the file to be served as a downloadable file
-		 * @param $arrData Multi-dimensional array of data to be converted into a CSV
+		 * @param $strFileName  Name of the file to be served as a downloadable file
+		 * @param $arrData      Multi-dimensional array of data to be converted into a CSV
 		 * @param $strDelimiter Delimiter to be used in creation of CSV data
 		 * @param $strEnclosure Enclosure to be used in creation of CSV data
 		 */
-		public function serve($strFileName,$arrData,$strDelimiter = ',',$strEnclosure = '"'){
+		public function serve( $strFileName, $arrData, $strDelimiter = ',', $strEnclosure = '"' ) {
 
-			$strOut = $this->generateCSV($arrData,$strDelimiter,$strEnclosure);
+			$strOut = $this->generateCSV( $arrData, $strDelimiter, $strEnclosure );
 
-			header("Content-type: text/csv");
-			header("Cache-Control: no-store, no-cache");
-			header(sprintf('Content-Disposition: attachment; filename="%s"',$strFileName));
+			header( "Content-type: text/csv" );
+			header( "Cache-Control: no-store, no-cache" );
+			header( sprintf( 'Content-Disposition: attachment; filename="%s"', $strFileName ) );
 
 			echo $strOut;
 			die();
@@ -73,16 +75,17 @@
 		/**
 		 * Generate the CSV data from a multi-dimensional array of data, ability to use a custom delimiter and enclosure.
 		 *
-		 * @param $arrData Multi-dimensional array of data to be converted into a CSV
+		 * @param $arrData      Multi-dimensional array of data to be converted into a CSV
 		 * @param $strDelimiter Delimiter to be used in creation of CSV data
 		 * @param $strEnclosure Enclosure to be used in creation of CSV data
+		 *
 		 * @return mixed Returns the CSV data as a string
 		 */
-		protected function generateCSV($arrData, $strDelimiter = ',', $strEnclosure = '"' ){
+		protected function generateCSV( $arrData, $strDelimiter = ',', $strEnclosure = '"' ) {
 
 			$resStream = fopen( 'php://temp/maxmemory', 'w+' );
 
-			fputcsv( $resStream, array_keys($arrData[0]), $strDelimiter, $strEnclosure );
+			fputcsv( $resStream, array_keys( $arrData[0] ), $strDelimiter, $strEnclosure );
 
 			foreach( $arrData as $arrRecord ) {
 				fputcsv( $resStream, $arrRecord, $strDelimiter, $strEnclosure );
@@ -98,28 +101,29 @@
 		/**
 		 * Pass in the local file path to a CSV file, the CSV file will be parsed and turned into an array. By default the Delimiter, Enclosure and Escape are already set.
 		 *
-		 * @param $strLocalFile Full path to the local CSV file that will be imported
-		 * @param $strLineDelimiter Expected delimiter for each line used in the CSV
+		 * @param $strLocalFile      Full path to the local CSV file that will be imported
+		 * @param $strLineDelimiter  Expected delimiter for each line used in the CSV
 		 * @param $intFieldDelimiter Expected delimiter for each field used in the CSV
-		 * @param $strEnclosure Expected enclosure to be used in creation of CSV data
-		 * @param $strEscape String used to escape the CSV data
+		 * @param $strEnclosure      Expected enclosure to be used in creation of CSV data
+		 * @param $strEscape         String used to escape the CSV data
+		 *
 		 * @return array Returns Multi-dimensional array of the CSV data
 		 */
-		public function import($strLocalFile, $strLineDelimiter = "\n", $intFieldDelimiter = ',', $strEnclosure = '"', $strEscape = '\\', $blUseFirstRowAsKeys = false){
+		public function import( $strLocalFile, $strLineDelimiter = "\n", $intFieldDelimiter = ',', $strEnclosure = '"', $strEscape = '\\', $blUseFirstRowAsKeys = false ) {
 
 			$arrOut = $arrHeaders = array();
 
-			foreach(str_getcsv(file_get_contents($strLocalFile), $strLineDelimiter) as $intRow => $strRow){
-				$arrRow = str_getcsv($strRow, $intFieldDelimiter, $strEnclosure, $strEscape);
-				if($blUseFirstRowAsKeys) {
-					if($intRow === 0) {
+			foreach( str_getcsv( file_get_contents( $strLocalFile ), $strLineDelimiter ) as $intRow => $strRow ) {
+				$arrRow = str_getcsv( $strRow, $intFieldDelimiter, $strEnclosure, $strEscape );
+				if( $blUseFirstRowAsKeys ) {
+					if( $intRow === 0 ) {
 						$arrHeaders = $arrRow;
 					} else {
-                        $arrRowIndexed=array();
-						foreach($arrRow as $intField => $mxdField){
-                            $arrRowIndexed[$arrHeaders[$intField]] = $mxdField;
-                        }
-                        $arrOut[] = $arrRowIndexed;
+						$arrRowIndexed = array();
+						foreach( $arrRow as $intField => $mxdField ) {
+							$arrRowIndexed[$arrHeaders[$intField]] = $mxdField;
+						}
+						$arrOut[] = $arrRowIndexed;
 					}
 				} else {
 					$arrOut[] = $arrRow;
