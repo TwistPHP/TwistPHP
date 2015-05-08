6b5bc9f3970a2bfd1106bbd2e13451e0faa5daaa
Dan Walker <danrwalker1@gmail.com>
2015-05-08 17:27:00 +0100

    Optimised the autoloader
    - Only loads files the first time around, also caches class paths in
    the twist/config folder for next time in a hope that this will speed
    the system up

--- src/twist/core/classes/Autoload.twist.php
+++ src/twist/core/classes/Autoload.twist.php
@@ -27,6 +27,9 @@
 
 		public $arrRegisteredLoaders = array();
 		public $strBaseDir = null;
+		public $arrClassLookup = array();
+		public $arrClassLoaded = array();
+		public $strAutoloadCache = null;
 
 		public static $resAutoLoader;
 
@@ -62,6 +65,8 @@
 		}
 
 		public function __construct(){
+			$this->strAutoloadCache = sprintf('%s/../../config/autoload.cache',dirname(__FILE__));
+			$this->arrClassLookup = json_decode(file_get_contents($this->strAutoloadCache),true);
 			spl_autoload_register(array($this,'load'));
 		}
 
@@ -74,54 +79,68 @@
 				$strRequest = sprintf('%s\\%s',__NAMESPACE__,$strRequest);
 			}
 
-			foreach($this->arrRegisteredLoaders as $strMatch => $arrLoader){
+			if(!array_key_exists($strRequest,$this->arrClassLoaded) && array_key_exists($strRequest,$this->arrClassLookup) && file_exists($this->arrClassLookup[$strRequest])){
+				require_once $this->arrClassLookup[$strRequest];
+			}elseif(!array_key_exists($strRequest,$this->arrClassLoaded)){
 
-				$regxMatchURI = null;
-				if(strstr($strMatch,'{') && strstr($strMatch,'}')){
-					$regxMatchNamespace = sprintf("#^(?<autoload_reg>%s)#i",str_replace(array('*',"\\","{","}"),array(".+","\\\\","(?<al_",">[^\\\\]+)",""),$strMatch));
-				}else{
-					$regxMatchNamespace = sprintf("#^%s(.*)#",str_replace(array('*','\\'),array('.+','\\\\'),$strMatch));
+				//If in lookup and not exists then kill
+				if(array_key_exists($strRequest,$this->arrClassLookup) && !file_exists($this->arrClassLookup[$strRequest])){
+					$this->arrClassLookup = array();
+					unlink($this->strAutoloadCache);
 				}
 
-				//Find a match for the file to be auto loaded
-				if(preg_match($regxMatchNamespace,$strRequest,$arrMatches)){
+				foreach($this->arrRegisteredLoaders as $strMatch => $arrLoader){
 
-					$blMatchFound = true;
-
-					if($arrLoader['type'] == 'path'){
-
-						//This is abit messy but allow {brackets} in auto loaders as replacements from the match to the string
-						//Using this method the file name must be caught at the end in a tag and place at the end of the path
-						if(array_key_exists('autoload_reg',$arrMatches)){
-
-							foreach($arrMatches as $strKey => $strValue){
-								if(substr($strKey,0,3) == 'al_'){
-									$strFind = "{".trim(substr($strKey,3))."}";
-									$arrLoader['path'] = str_replace($strFind,$strValue,$arrLoader['path']);
-								}
-							}
-
-							$strRequireFile = sprintf('%s%s',$arrLoader['path'],$arrLoader['extension']);
-						}else{
-							$strRequireFile = sprintf('%s/%s%s',$arrLoader['path'],str_replace('\\','/',$arrMatches[1]),$arrLoader['extension']);
-						}
-
-						if(file_exists($strRequireFile)){
-							require_once $strRequireFile;
-						}else{
-							$blMatchFound = false;
-						}
+					$regxMatchURI = null;
+					if(strstr($strMatch,'{') && strstr($strMatch,'}')){
+						$regxMatchNamespace = sprintf("#^(?<autoload_reg>%s)#i",str_replace(array('*',"\\","{","}"),array(".+","\\\\","(?<al_",">[^\\\\]+)",""),$strMatch));
 					}else{
-						//Only send the handle
-						call_user_func($arrLoader['class'],$strRequest);
+						$regxMatchNamespace = sprintf("#^%s(.*)#",str_replace(array('*','\\'),array('.+','\\\\'),$strMatch));
 					}
 
-					break;
-				}
-			}
+					//Find a match for the file to be auto loaded
+					if(preg_match($regxMatchNamespace,$strRequest,$arrMatches)){
 
-			if($blMatchFound == false){
-				throw new \Exception(sprintf("TwistPHP AutoLoader: Unable to load the requested class '%s', please check to see if the file exists",$strRequest));
+						$blMatchFound = true;
+
+						if($arrLoader['type'] == 'path'){
+
+							//This is abit messy but allow {brackets} in auto loaders as replacements from the match to the string
+							//Using this method the file name must be caught at the end in a tag and place at the end of the path
+							if(array_key_exists('autoload_reg',$arrMatches)){
+
+								foreach($arrMatches as $strKey => $strValue){
+									if(substr($strKey,0,3) == 'al_'){
+										$strFind = "{".trim(substr($strKey,3))."}";
+										$arrLoader['path'] = str_replace($strFind,$strValue,$arrLoader['path']);
+									}
+								}
+
+								$strRequireFile = sprintf('%s%s',$arrLoader['path'],$arrLoader['extension']);
+							}else{
+								$strRequireFile = sprintf('%s/%s%s',$arrLoader['path'],str_replace('\\','/',$arrMatches[1]),$arrLoader['extension']);
+							}
+
+							if(file_exists($strRequireFile)){
+								require_once $strRequireFile;
+								$this->arrClassLookup[$strRequest] = $strRequireFile;
+								$this->arrClassLoaded[$strRequest] = $strRequireFile;
+								file_put_contents($this->strAutoloadCache,json_encode($this->arrClassLookup));
+							}else{
+								$blMatchFound = false;
+							}
+						}else{
+							//Only send the handle
+							call_user_func($arrLoader['class'],$strRequest);
+						}
+
+						break;
+					}
+				}
+
+				if($blMatchFound == false){
+					throw new \Exception(sprintf("TwistPHP AutoLoader: Unable to load the requested class '%s', please check to see if the file exists",$strRequest));
+				}
 			}
 		}
 	}
