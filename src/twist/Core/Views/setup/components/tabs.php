801d7e2cf8d2bc0898ccecdb1b77f3161b943082
Dan Walker <danrwalker1@gmail.com>
2015-05-18 16:08:58 +0100

    Update to fix setup tabs

--- src/twist/Core/Views/setup/components/tabs.php
+++ src/twist/Core/Views/setup/components/tabs.php
@@ -2,41 +2,43 @@
 
     $intRouteKey = 0;
 
-    if(count($_SERVER['TWIST_ROUTE_PARTS']) == 0){
-        $_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] = 'welcome';
-    }
+    $arrRoute = \Twist::Route()->current();
 
-    $arrOptions = array(
+	if(count($arrRoute['parts']) == 0){
+		$arrRoute['parts'][$intRouteKey] = 'welcome';
+	}
+
+	$arrOptions = array(
 		'licence' => array(
-			'disabled' => ($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] != 'licence') ? 'disabled' : '',
-			'current' => ($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] == 'licence') ? 'current' : '',
-			'icon' => (in_array($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey],array('welcome','licence'))) ? 'fa-circle-o' : 'fa-check-circle-o'
+			'disabled' => ($arrRoute['parts'][$intRouteKey] != 'licence') ? 'disabled' : '',
+			'current' => ($arrRoute['parts'][$intRouteKey] == 'licence') ? 'current' : '',
+			'icon' => (in_array($arrRoute['parts'][$intRouteKey],array('welcome','licence'))) ? 'fa-circle-o' : 'fa-check-circle-o'
 		),
-        'checks' => array(
-            'disabled' => ($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] != 'checks') ? 'disabled' : '',
-            'current' => ($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] == 'checks') ? 'current' : '',
-            'icon' => (in_array($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey],array('welcome','licence','checks'))) ? 'fa-circle-o' : 'fa-check-circle-o'
-        ),
-        'database' => array(
-            'disabled' => ($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] != 'database') ? 'disabled' : '',
-            'current' => ($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] == 'database') ? 'current' : '',
-            'icon' => (in_array($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey],array('welcome','licence','checks','database'))) ? 'fa-circle-o' : 'fa-check-circle-o'
-        ),
-        'settings' => array(
-            'disabled' => ($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] != 'settings') ? 'disabled' : '',
-            'current' => ($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] == 'settings') ? 'current' : '',
-            'icon' => (in_array($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey],array('welcome','licence','checks','database','settings'))) ? 'fa-circle-o' : 'fa-check-circle-o'
-        ),
-        'user' => array(
-            'disabled' => ($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] != 'user') ? 'disabled' : '',
-            'current' => ($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] == 'user') ? 'current' : '',
-            'icon' => (in_array($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey],array('welcome','licence','checks','database','settings','user'))) ? 'fa-circle-o' : 'fa-check-circle-o'
-        ),
-        'interfaces' => array(
-            'disabled' => ($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] != 'interfaces') ? 'disabled' : '',
-            'current' => ($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey] == 'interfaces') ? 'current' : '',
-            'icon' => (in_array($_SERVER['TWIST_ROUTE_PARTS'][$intRouteKey],array('welcome','licence','checks','database','settings','user','interfaces'))) ? 'fa-circle-o' : 'fa-check-circle-o'
-        )
-    );
+		'checks' => array(
+			'disabled' => ($arrRoute['parts'][$intRouteKey] != 'checks') ? 'disabled' : '',
+			'current' => ($arrRoute['parts'][$intRouteKey] == 'checks') ? 'current' : '',
+			'icon' => (in_array($arrRoute['parts'][$intRouteKey],array('welcome','licence','checks'))) ? 'fa-circle-o' : 'fa-check-circle-o'
+		),
+		'database' => array(
+			'disabled' => ($arrRoute['parts'][$intRouteKey] != 'database') ? 'disabled' : '',
+			'current' => ($arrRoute['parts'][$intRouteKey] == 'database') ? 'current' : '',
+			'icon' => (in_array($arrRoute['parts'][$intRouteKey],array('welcome','licence','checks','database'))) ? 'fa-circle-o' : 'fa-check-circle-o'
+		),
+		'settings' => array(
+			'disabled' => ($arrRoute['parts'][$intRouteKey] != 'settings') ? 'disabled' : '',
+			'current' => ($arrRoute['parts'][$intRouteKey] == 'settings') ? 'current' : '',
+			'icon' => (in_array($arrRoute['parts'][$intRouteKey],array('welcome','licence','checks','database','settings'))) ? 'fa-circle-o' : 'fa-check-circle-o'
+		),
+		'user' => array(
+			'disabled' => ($arrRoute['parts'][$intRouteKey] != 'user') ? 'disabled' : '',
+			'current' => ($arrRoute['parts'][$intRouteKey] == 'user') ? 'current' : '',
+			'icon' => (in_array($arrRoute['parts'][$intRouteKey],array('welcome','licence','checks','database','settings','user'))) ? 'fa-circle-o' : 'fa-check-circle-o'
+		),
+		'interfaces' => array(
+			'disabled' => ($arrRoute['parts'][$intRouteKey] != 'interfaces') ? 'disabled' : '',
+			'current' => ($arrRoute['parts'][$intRouteKey] == 'interfaces') ? 'current' : '',
+			'icon' => (in_array($arrRoute['parts'][$intRouteKey],array('welcome','licence','checks','database','settings','user','interfaces'))) ? 'fa-circle-o' : 'fa-check-circle-o'
+		)
+	);
 
-    echo \Twist::View()->build('components/tabs.tpl',$arrOptions);
+	echo \Twist::View()->build('components/tabs.tpl',$arrOptions);
