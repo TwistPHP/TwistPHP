250d47977a87dd6eeefecbb2383089367ca00651
Andrew Hosgood <andi@floor44.co.uk>
2015-07-03 22:53:01 +0100

    Initial commit for optional JS error logging
    Eventually to be displayed in the manager alongside PHP errors

---
+++ src/twist/Core/Resources/twist/error-monitor/js/error-monitor.js
@@ -0,0 +1,37 @@
+/**
+ * This file is part of TwistPHP.
+ *
+ * TwistPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TwistPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TwistPHP.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author     Shadow Technologies Ltd. <contact@shadow-technologies.co.uk>
+ * @license    https://www.gnu.org/licenses/gpl.html LGPL License
+ * @link       https://twistphp.com
+ *
+ */
+
+(
+	function( window ) {
+		var funOriginalError = window.onerror;
+
+		window.onerror = function( strErrorMessage, strURL, intLineNumber ) {
+			//LOG ERROR TO TWIST
+
+			if( funOriginalError ) {
+				return funOriginalError( strErrorMessage, strURL, intLineNumber );
+			}
+
+			return false;
+		}
+	}
+)();
