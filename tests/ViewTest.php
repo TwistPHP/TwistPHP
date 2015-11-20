db958a4eb04ad5761d92b084135be38d3fa3ae6a
Dan Walker <danrwalker1@gmail.com>
2015-11-20 02:35:16 +0000

    view tests

--- tests/ViewTest.php
+++ tests/ViewTest.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace App\Models;
+require_once './index.php';
 
 class ViewTest extends \PHPUnit_Framework_TestCase{
 
 	public function testReplaceTag(){
-		//$this -> assertEquals('completes',\Twist::View()->replace('{data:test}',array('test' => 'complete')));
-		$this -> assertEquals('completes','complete');
+		$this -> assertEquals('complete',\Twist::View()->replace('{data:test}',array('test' => 'complete')));
+	}
+
+	public function testReplaceTag2(){
+		$this -> assertEquals('completes',\Twist::View()->replace('{data:test}',array('test' => 'complete')));
 	}
 }
