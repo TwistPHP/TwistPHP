0affbdd88dfb9d607477e8c7806eb24d35770752
Dan <danrwalker1@gmail.com>
2015-12-15 16:17:53 +0000

    More unit test updates

--- tests/Settings.php
+++ tests/Settings.php
@@ -7,4 +7,12 @@ class Settings extends \PHPUnit_Framework_TestCase{
 	public function testGet(){
 		$this -> assertEquals('Travis CI Test',\Twist::framework()->setting('SITE_NAME'));
 	}
+
+	public function testSet(){
+		$this -> assertTrue(\Twist::framework()->setting('SITE_NAME','Travis CI Test - Updated'));
+		$this -> assertEquals('Travis CI Test - Updated',\Twist::framework()->setting('SITE_NAME'));
+
+		//Reset the site title back to its original setting - not a test
+		\Twist::framework()->setting('SITE_NAME','Travis CI Test');
+	}
 }
