4a880165ff6a6d7e6bb39e61ff13c574e919f2ea
Andrew Hosgood <andi@floor44.co.uk>
2016-01-26 23:23:02 +0000

    Update to debug JS - better logging

--- dist/twist/Core/Views/debug/_base.tpl
+++ dist/twist/Core/Views/debug/_base.tpl
@@ -1,4 +1,3 @@
-<!--<script src="https://www.google.com/jsapi"></script>-->
 <div id="twist-debug">
 	<a href="https://twistphp.com/docs" target="_blank" id="twist-debug-icon" title="TwistPHP documentation"><img src="{resource:core-uri}twist/logos/logo-64.png" alt="TwistPHP documentation"></a>
 	<ul id="twist-debug-blocks">
--- src/Core/Resources/twist/debug/js/twist-debug.js
+++ src/Core/Resources/twist/debug/js/twist-debug.js
@@ -18,322 +18,247 @@
  * @license    https://www.gnu.org/licenses/gpl.html LGPL License
  * @link       https://twistphp.com
  *
- * --------------
  * TwistPHP Debug
  * --------------
+ * @version 0.9.0
  */
 
 (
-		(function( root, factory ) {
-			if( typeof define === 'function' &&
-					define.amd ) {
-				define(
-						'twistdebug',
-						['postal'],
-						function( postal ) {
-							return ( root.twistdebug = factory( postal ) );
-						}
-				);
-			} else if( typeof module === 'object' &&
-					module.exports ) {
-				module.exports = ( root.twistdebug = factory( require( 'postal' ) ) );
-			} else {
-				root.twistdebug = factory( root.postal );
-			}
-		})(
-				this,
-				function( postal ) {
-					var TwistDebug = function() {
-						var arrThingsToLog = [],
-								logJavaScriptError = function( strColour, strTitle, strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
-									var objToStack = {
-										colour: strColour,
-										title: strTitle,
-										message: strErrorMessage,
-										url: strURL,
-										line: intLineNumber,
-										column: intColumn,
-										error: objError
-									};
-
-									arrThingsToLog.push( objToStack );
-								};
-
-
-						window.onerror = function( strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
-							logJavaScriptError( 'red', 'JS Error', strErrorMessage, strURL, intLineNumber, intColumn, objError );
-
-							return true;
-						};
-
-						this.log = function() {
-							if( window.console &&
-									window.console.log &&
-									arguments.length ) {
-								for( var intArguement in arguments ) {
-									window.console.log( arguments[intArguement] );
-								}
-							}
-						};
-
-						this.info = function() {
-							if( window.console &&
-									arguments.length ) {
-								for( var intArguement in arguments ) {
-									if( window.console.info ) {
-										window.console.info( arguments[intArguement] );
-									} else {
-										log( 'INFO: ', arguments[intArguement] );
-									}
-								}
-							}
-						};
-
-						this.error = function() {
-							if( window.console &&
-									arguments.length ) {
-								for( var intArguement in arguments ) {
-									if( window.console.error ) {
-										window.console.error( arguments[intArguement] );
-									} else {
-										log( 'ERROR: ', arguments[intArguement] );
-									}
-								}
-							}
-						};
-
-						this.warn = function() {
-							if( window.console &&
-									arguments.length ) {
-								for( var intArguement in arguments ) {
-									if( window.console.warn ) {
-										window.console.warn( arguments[intArguement] );
-									} else {
-										log( 'WARNING: ', arguments[intArguement] );
-									}
-								}
-							}
-						};
+	function( window, document ) {
+		var arrThingsToLog = { errors: [], warnings: [], logs: [] },
+				logErrorToDebugConsole = function( strTitle, strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
+					var objToStack = {
+						title: strTitle,
+						message: strErrorMessage,
+						url: strURL,
+						line: intLineNumber,
+						column: intColumn,
+						error: objError
 					};
 
-					return TwistDebug;
-				}
-		)
-);
+					arrThingsToLog.errors.push( objToStack );
+				},
+				logWarningToDebugConsole = function( strTitle, strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
+					var objToStack = {
+						title: strTitle,
+						message: strErrorMessage,
+						url: strURL,
+						line: intLineNumber,
+						column: intColumn,
+						error: objError
+					};
 
+					arrThingsToLog.warnings.push( objToStack );
+				},
+				logToDebugConsole = function( strTitle, strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
+					var objToStack = {
+						title: strTitle,
+						message: strErrorMessage,
+						url: strURL,
+						line: intLineNumber,
+						column: intColumn,
+						error: objError
+					};
 
-(function( window, document ) {
-	var arrThingsToLog = [],
-			logToDebugConsole = function( strColour, strTitle, strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
-				var objToStack = {
-					colour: strColour,
-					title: strTitle,
-					message: strErrorMessage,
-					url: strURL,
-					line: intLineNumber,
-					column: intColumn,
-					error: objError
+					arrThingsToLog.logs.push( objToStack );
+				},
+				/*log = function() {
+					if( window.console &&
+							window.console.log &&
+							arguments.length ) {
+						for( var intArguement in arguments ) {
+							window.console.log( arguments[intArguement] );
+						}
+					}
+				},
+				info = function() {
+					if( window.console &&
+							arguments.length ) {
+						for( var intArguement in arguments ) {
+							if( window.console.info ) {
+								window.console.info( arguments[intArguement] );
+							} else {
+								log( 'INFO: ', arguments[intArguement] );
+							}
+						}
+					}
+				},
+				error = function() {
+					if( window.console &&
+							arguments.length ) {
+						for( var intArguement in arguments ) {
+							if( window.console.error ) {
+								window.console.error( arguments[intArguement] );
+							} else {
+								log( 'ERROR: ', arguments[intArguement] );
+							}
+						}
+					}
+				},
+				warn = function() {
+					if( window.console &&
+							arguments.length ) {
+						for( var intArguement in arguments ) {
+							if( window.console.warn ) {
+								window.console.warn( arguments[intArguement] );
+							} else {
+								log( 'WARNING: ', arguments[intArguement] );
+							}
+						}
+					}
+				},*/
+				funOriginalWindowError = window.onerror || function() {},
+				blUseOriginalError = false;
+
+		window.onerror = function( strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
+					logErrorToDebugConsole( 'JS Error', strErrorMessage, strURL, intLineNumber, intColumn, objError );
+
+					return true;
 				};
 
-				arrThingsToLog.push( objToStack );
-			},
-			log = function() {
-				if( window.console &&
-						window.console.log &&
-						arguments.length ) {
-					for( var intArguement in arguments ) {
-						window.console.log( arguments[intArguement] );
-					}
-				}
-			},
-			info = function() {
-				if( window.console &&
-						arguments.length ) {
-					for( var intArguement in arguments ) {
-						if( window.console.info ) {
-							window.console.info( arguments[intArguement] );
-						} else {
-							log( 'INFO: ', arguments[intArguement] );
+		var blOtherJSLibrary = false,
+				getScript = function( strURL, funSuccess ) {
+					var domScript = document.createElement( 'script' ),
+							domHead = document.getElementsByTagName( 'head' )[0],
+							blDone = false;
+
+					funSuccess = ( typeof funSuccess === 'function' ) ? funSuccess : function() {};
+
+					domScript.src = strURL,
+					domScript.onload = domScript.onreadystatechange = function() {
+						if( !blDone &&
+								( !this.readyState ||
+									this.readyState === 'loaded' ||
+									this.readyState === 'complete' ) ) {
+							blDone = true;
+							try {
+								funSuccess();
+							} catch( err ) {
+								error( err );
+							}
+							domScript.onload = domScript.onreadystatechange = null;
+							domHead.removeChild( domScript );
 						}
+					};
+
+					domHead.appendChild( domScript );
+				},
+				TwistDebug = function( blNoConfilct ) {
+					/* ===================================================== GO, GO, GO! ===================================================== */
+					var $ = ( blNoConfilct === true ) ? window.jQuery.noConflict( true ) : window.jQuery,
+							jqoTwistDebugBlocks = $( '#twist-debug-blocks' ),
+							jqoTwistDebugDetails = $( '#twist-debug-details' ),
+							logDebug = function( strColour, strTitle, strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
+								var jqoLog = $( '<p/>' ).html( '<strong>' + strTitle + ':</strong> ' + strErrorMessage );
+
+								$( '#twist-debug-messages' ).find( '.twist-debug-column-wrapper' ).append( jqoLog );
+
+								jqoLog.wrap( '<div class="twist-debug-column-100"/>' ).wrap( '<div class="twist-debug-box-' + strColour + ' twist-debug-message" data-title="' + strURL + ', line ' + intLineNumber + '"/>' );
+							};
+
+					info( 'jQuery v.' + $.fn.jquery + ' is ready' );
+
+					logErrorToDebugConsole = function( strTitle, strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
+						logDebug( 'red', strTitle, strErrorMessage, strURL, intLineNumber, intColumn, objError );
+
+						var jqoErrorCount = $( '#twist-debug-errors' );
+
+						jqoErrorCount.html( jqoErrorCount.find( 'i' )[0].outerHTML + ( parseInt( jqoErrorCount.text() ) + 1 ) ).removeClass( 'twist-debug-hidden' );
+					};
+
+					logWarningToDebugConsole = function( strTitle, strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
+						logDebug( 'yellow', strTitle, strErrorMessage, strURL, intLineNumber, intColumn, objError );
+
+						var jqoErrorCount = $( '#twist-debug-warnings' );
+
+						jqoErrorCount.html( jqoErrorCount.find( 'i' )[0].outerHTML + ( parseInt( jqoErrorCount.text() ) + 1 ) ).removeClass( 'twist-debug-hidden' );
+					};
+
+					logToDebugConsole = function( strTitle, strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
+						logDebug( 'blue', strTitle, strErrorMessage, strURL, intLineNumber, intColumn, objError );
+
+						var jqoErrorCount = $( '#twist-debug-dumps' );
+
+						jqoErrorCount.html( jqoErrorCount.find( 'i' )[0].outerHTML + ( parseInt( jqoErrorCount.text() ) + 1 ) ).removeClass( 'twist-debug-hidden' );
+					};
+
+					for( var intStackedLog in arrThingsToLog.errors ) {
+						var objLog = arrThingsToLog[intStackedLog];
+						logErrorToDebugConsole( objLog.title, objLog.message, objLog.url, objLog.line, objLog.column, objLog.error );
 					}
-				}
-			},
-			error = function() {
-				if( window.console &&
-						arguments.length ) {
-					for( var intArguement in arguments ) {
-						if( window.console.error ) {
-							window.console.error( arguments[intArguement] );
-						} else {
-							log( 'ERROR: ', arguments[intArguement] );
-						}
+
+					for( var intStackedLog in arrThingsToLog.warnings ) {
+						var objLog = arrThingsToLog[intStackedLog];
+						logWarningToDebugConsole( objLog.title, objLog.message, objLog.url, objLog.line, objLog.column, objLog.error );
 					}
-				}
-			},
-			warn = function() {
-				if( window.console &&
-						arguments.length ) {
-					for( var intArguement in arguments ) {
-						if( window.console.warn ) {
-							window.console.warn( arguments[intArguement] );
-						} else {
-							log( 'WARNING: ', arguments[intArguement] );
-						}
+
+					for( var intStackedLog in arrThingsToLog.logs ) {
+						var objLog = arrThingsToLog[intStackedLog];
+						logToDebugConsole( objLog.title, objLog.message, objLog.url, objLog.line, objLog.column, objLog.error );
 					}
-				}
-			},
-			funOriginalWindowError = window.onerror || function() {},
-			blUseOriginalError = false;
 
-	window.onerror = function( strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
-		logToDebugConsole( 'red', 'JS Error', strErrorMessage, strURL, intLineNumber, intColumn, objError );
-
-		return true;
-	};
-
-	var blOtherJSLibrary = false,
-			getScript = function( strURL, funSuccess ) {
-				var domScript = document.createElement( 'script' ),
-						domHead = document.getElementsByTagName( 'head' )[0],
-						blDone = false;
-
-				funSuccess = ( typeof funSuccess === 'function' ) ? funSuccess : function() {};
-
-				domScript.src = strURL;
-				domScript.onload = domScript.onreadystatechange = function() {
-					if( !blDone &&
-							( !this.readyState ||
-							this.readyState === 'loaded' ||
-							this.readyState === 'complete' ) ) {
-						blDone = true;
-						try {
-							funSuccess();
-						} catch( err ) {
-							error( err );
-						}
-						domScript.onload = domScript.onreadystatechange = null;
-						domHead.removeChild( domScript );
-					}
-				};
-
-				domHead.appendChild( domScript );
-			},
-			loadDebugger = function( blNoConfilct ) {
-				/* ===================================================== GO, GO, GO! ===================================================== */
-				var $ = ( blNoConfilct === true ) ? window.jQuery.noConflict( true ) : window.jQuery,
-						blMemoryChartLoaded = false,
-						jqoTwistDebugBlocks = $( '#twist-debug-blocks' ),
-						jqoTwistDebugDetails = $( '#twist-debug-details' );
-
-				info( 'jQuery v.' + $.fn.jquery + ' is ready' );
-
-				logToDebugConsole = function( strTitle, strColour, strErrorMessage, strURL, intLineNumber, intColumn, objError ) {
-					var jqoLog = $( '<p/>' ).html( '<strong>' + strTitle + ':</strong> ' + strErrorMessage );
-
-					$( '#twist-debug-messages' ).find( '.twist-debug-column-wrapper' ).append( jqoLog );
-
-					jqoLog.wrap( '<div class="twist-debug-column-100"/>' ).wrap( '<div class="twist-debug-box-' + strColour + ' twist-debug-message" data-title="' + strURL + ', line ' + intLineNumber + '"/>' );
-
-					var jqoErrorCount = $( '#twist-debug-errors' );
-
-					jqoErrorCount.html( jqoErrorCount.find( 'i' )[0].outerHTML + ( parseInt( jqoErrorCount.text() ) + 1 ) ).removeClass( 'twist-debug-hidden' );
-				};
-
-				for( var intStackedLog in arrThingsToLog ) {
-					logToDebugConsole( arrThingsToLog[intStackedLog].colour, arrThingsToLog[intStackedLog].title, arrThingsToLog[intStackedLog].message, arrThingsToLog[intStackedLog].url, arrThingsToLog[intStackedLog].line, arrThingsToLog[intStackedLog].column, arrThingsToLog[intStackedLog].error );
-				}
-
-				$( '.twist-debug-box, [class^="twist-debug-box-"], [class*=" twist-debug-box-"]' ).has( '.twist-debug-more-details' ).each(
+					$( '.twist-debug-box, [class^="twist-debug-box-"], [class*=" twist-debug-box-"]' ).has( '.twist-debug-more-details' ).each(
 						function() {
 							var jqoMoreDetails = $( this ).find( '.twist-debug-more-details' );
 
 							jqoMoreDetails.after( '<a href="#twist-debug-more-details" class="twist-debug-more-details">&ctdot;</a>' );
 						}
-				);
-				jqoTwistDebugBlocks.on( 'click', 'a',
+					),
+					jqoTwistDebugBlocks.on( 'click', 'a',
 						function( e ) {
 							e.preventDefault();
 							var jqoThisBlock = $( this );
 							if( jqoThisBlock.hasClass( 'current' ) ) {
-								jqoTwistDebugDetails.removeClass( 'show' );
+								jqoTwistDebugDetails.removeClass( 'show' ),
 								jqoThisBlock.removeClass( 'current' );
 							} else {
 								var jqsTarget = jqoThisBlock.attr( 'href' );
 
-								jqoTwistDebugDetails.addClass( 'show' ).children( 'div' ).hide().filter( jqsTarget ).show();
-								jqoTwistDebugBlocks.find( 'a.current' ).removeClass( 'current' );
+								jqoTwistDebugDetails.addClass( 'show' ).children( 'div' ).hide().filter( jqsTarget ).show(),
+								jqoTwistDebugBlocks.find( 'a.current' ).removeClass( 'current' ),
 								jqoThisBlock.addClass( 'current' );
-
-								if( !blMemoryChartLoaded &&
-										jqsTarget === '#twist-debug-memory' ) {
-									/*google.load('visualization', '1.1', {packages: ['line']});
-									 var data = new google.visualization.DataTable();
-
-									 data.addColumn( 'number', 'Time' );
-									 data.addColumn( 'number', 'Memory (MB)' );
-									 data.addRows(
-									 [
-									 [1, 37.8],
-									 [2, 30.9],
-									 [3, 25.4],
-									 [6, 8.8],
-									 [7, 7.6],
-									 [8, 12.3],
-									 [10,12.8],
-									 [11, 5.3],
-									 [14, 4.2]
-									 ]
-									 );
-
-									 var chart = new google.charts.Line( document.getElementById( 'twist-debug-memory-chart' ) );
-
-									 chart.draw( data,
-									 {
-									 chart: { title: 'Memory Usage' },
-									 width: 900,
-									 height: 500
-									 }
-									 );*/
-
-									blMemoryChartLoaded = true;
-								}
 							}
 						}
-				);
-				jqoTwistDebugDetails.on( 'click', 'a[href="#close-twist-debug-details"]',
+					),
+					jqoTwistDebugDetails.on( 'click', 'a[href="#close-twist-debug-details"]',
 						function( e ) {
 							e.preventDefault();
-							jqoTwistDebugBlocks.find( 'a.current' ).removeClass( 'current' );
+							jqoTwistDebugBlocks.find( 'a.current' ).removeClass( 'current' ),
 							jqoTwistDebugDetails.removeClass( 'show' );
 						}
-				).on( 'click', 'a[href="#twist-debug-more-details"]',
+					).on( 'click', 'a[href="#twist-debug-more-details"]',
 						function( e ) {
 							e.preventDefault();
 
 							$( this ).prev( '.twist-debug-more-details' ).slideToggle();
 						}
-				);
-			};
+					);
 
-	if( typeof window.jQuery === 'undefined' ) {
-		blOtherJSLibrary = ( typeof window.$ === 'function' );
+					this.log = logErrorToDebugConsole;
+					
+					return this;
+				};
 
-		getScript( 'http' + ( location.protocol === 'https:' ? 's' : '' ) + '://code.jquery.com/jquery-1.12.0.min.js',
+		if( typeof window.jQuery === 'undefined' ) {
+			blOtherJSLibrary = ( typeof window.$ === 'function' );
+
+			getScript( 'http' + ( location.protocol === 'https:' ? 's' : '' ) + '://code.jquery.com/jquery-1.12.0.min.js',
 				function() {
 					if( typeof window.jQuery === 'undefined' ) {
 						error( 'This is embarrassing... jQuery couldn\'t be loaded' );
 					} else {
 						if( !blOtherJSLibrary ) {
-							loadDebugger( false );
+							window.twistdebug = new TwistDebug( false );
 						} else {
 							warn( 'Another JS library controls $' );
-							loadDebugger( true );
+							window.twistdebug = new TwistDebug( true );
 						}
 					}
 				}
-		);
-	} else {
-		info( 'jQuery v.' + $.fn.jquery + ' exists' );
-		loadDebugger( false );
+			);
+		} else {
+			info( 'jQuery v.' + $.fn.jquery + ' exists' );
+			window.twistdebug = new TwistDebug( false );
+		}
 	}
-})( window, document );
+)( window, document );
