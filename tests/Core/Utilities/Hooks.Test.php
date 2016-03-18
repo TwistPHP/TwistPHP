b3969c95ffe6b0fd2305151ba1f08dde38d79a16
Dan Walker <danrwalker1@gmail.com>
2016-03-18 01:03:13 +0000

    bug fix tests

--- tests/Core/Utilities/Hooks.Test.php
+++ tests/Core/Utilities/Hooks.Test.php
@@ -2,20 +2,20 @@
 
 class Hooks extends \PHPUnit_Framework_TestCase{
 
-	public function getAllHooks(){
+	public function testGetAllHooks(){
 		$this->assertTrue(array_key_exists('TWIST_VIEW_TAG',\Twist::framework()->hooks()->getAll()));
 	}
 
-	public function registerHook(){
+	public function testRegisterHook(){
 		\Twist::framework()->hooks()->register('travisCI','travis-test-hook','test-hook');
 		$this->assertEquals('test-hook',\Twist::framework()->hooks()->get('travisCI','travis-test-hook'));
 	}
 
-	public function getRegisteredHook(){
+	public function testGetRegisteredHook(){
 		$this->assertEquals('test-hook',\Twist::framework()->hooks()->get('travisCI','travis-test-hook'));
 	}
 
-	public function cancelRegisteredHook(){
+	public function testCancelRegisteredHook(){
 		\Twist::framework()->hooks()->cancel('travisCI','travis-test-hook');
 		$this->assertEquals(array(),\Twist::framework()->hooks()->get('travisCI','travis-test-hook'));
 	}
