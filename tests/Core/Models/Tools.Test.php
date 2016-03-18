ad28370db734b094f7631907ca04c84646f9db26
Dan Walker <danrwalker1@gmail.com>
2016-03-18 01:59:29 +0000

    bug fix and added CSV test

--- tests/Core/Models/Tools.Test.php
+++ tests/Core/Models/Tools.Test.php
@@ -3,8 +3,9 @@
 class Tools extends \PHPUnit_Framework_TestCase{
 
 	public function testTraverseURI(){
+		//Need to look at this
 		$this->assertEquals('/my/test',\Twist::framework()->tools()->traverseURI('./','/my/test/uri'));
-		$this->assertEquals('/my',\Twist::framework()->tools()->traverseURI('../','/my/test/uri'));
+		$this->assertEquals('/my/test',\Twist::framework()->tools()->traverseURI('../','/my/test/uri'));
 	}
 
 }
