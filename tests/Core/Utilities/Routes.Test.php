1f5fccab579548796a8ad2d3323bb2c263a7db01
Dan Walker <danrwalker1@gmail.com>
2017-05-11 12:38:34 +0100

    More Debug of the Rest user test

--- tests/Core/Utilities/Routes.Test.php
+++ tests/Core/Utilities/Routes.Test.php
@@ -114,11 +114,12 @@ class Routes extends \PHPUnit_Framework_TestCase{
 		$this->assertContains('<status>success</status>', $strResponseXML);
 
 		//Test before login
+		$this -> assertEquals('error',$this->simulateAPIRequest('/test-userapi-controller/test',$strAPIKey));
 		$arrRESTResponse = json_decode($this->simulateAPIRequest('/test-userapi-controller/test',$strAPIKey),true);
 		//$this -> assertEquals('error',$arrRESTResponse['status']);
 
 		//Test with user
-		$arrRESTResponse = json_decode($this->simulateAPIRequest('/test-userapi-controller/test',$strAPIKey,'travisci@unit-test-rest-twistphp.com','X123Password'),true);
+		$arrRESTResponse = json_decode($this->simulateAPIRequest('/test-userapi-controller/connect',$strAPIKey,'travisci@unit-test-rest-twistphp.com','X123Password'),true);
 		$this -> assertEquals('success',$arrRESTResponse['status']);
 		$this -> assertTrue(array_key_exists('auth_token',$arrRESTResponse['results']));
 
