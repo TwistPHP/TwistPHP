a3962366309d6a4a399d9ec5dfbe2b8a43e79006
Dan Walker <danrwalker1@gmail.com>
2017-05-17 14:38:48 +0100

    Added some tests for the email models

---
+++ tests/Core/Utilities/Email.Test.php
@@ -0,0 +1,43 @@
+<?php
+
+class Email extends \PHPUnit_Framework_TestCase{
+
+	public static $arrSource = array();
+
+	public function testCreate(){
+
+		$resEmail = \Twist::Email()->create();
+		$resEmail->setEncoding();
+		$resEmail->setCharEncoding();
+		$resEmail->setSensitivity();
+		$resEmail->setPriority();
+		$resEmail->addTo('travisci2@unit-test-twistphp.com','Travis CI 2');
+		$resEmail->addCc('travisci3@unit-test-twistphp.com','Travis CI 3');
+		$resEmail->addBcc('travisci4@unit-test-twistphp.com','Travis CI 4');
+		$resEmail->setFrom('travisci@unit-test-twistphp.com','Travis CI');
+		$resEmail->setSubject('A test email');
+		$resEmail->setBodyHTML('Body of a test email<br>From TwistPHP');
+		$resEmail->addAttachment(TWIST_APP.'/Data/test.json');
+
+		self::$arrSource = $resEmail->source();
+
+		$this->assertContains('travisci2@unit-test-twistphp.com',self::$arrSource['to']);
+		$this->assertEquals('A test email',self::$arrSource['subject']);
+		$this->assertContains('Body of a test email', self::$arrSource['body']);
+		$this->assertContains('From: Travis CI <travisci@unit-test-twistphp.com>', self::$arrSource['raw']);
+	}
+
+	public function testParser(){
+
+		$arrEmailData = \Twist::Email()->parseSource(self::$arrSource);
+
+		$this->assertEquals('travisci2@unit-test-twistphp.com',$arrEmailData['to']);
+		$this->assertEquals('travisci@unit-test-twistphp.com',$arrEmailData['from']);
+		$this->assertEquals('A test email',$arrEmailData['subject']);
+
+		$this->assertContains('Body of a test email',$arrEmailData['body']['plain']);
+		$this->assertContains('Body of a test email',$arrEmailData['body']['html']);
+
+		$this->assertTrue(count($arrEmailData['attachments']) > 0);
+	}
+}
