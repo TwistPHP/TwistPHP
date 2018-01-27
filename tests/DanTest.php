ce56df44e9c11b065b4861cf7125e2af700d26bc
Dan Walker <danrwalker1@gmail.com>
2018-01-27 11:13:58 +0000

    added test

---
+++ tests/DanTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Dan extends TestCase{
+
+	public function testDan(){
+		$this->assertTrue(false);
+	}
+
+	public function testDan2(){
+
+		$this->assertTrue(true);
+	}
+}
