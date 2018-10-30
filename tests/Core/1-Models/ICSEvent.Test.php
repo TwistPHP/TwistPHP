177add5394949b2a3f67df4a79a1c8156eaebb19
Dan Walker <danrwalker1@gmail.com>
2018-10-30 23:52:02 +0000

    Updates to test

--- tests/Core/1-Models/ICSEvent.Test.php
+++ tests/Core/1-Models/ICSEvent.Test.php
@@ -16,11 +16,11 @@
 
 			$this->assertNull($resEvent->startDate());
 			$resEvent->startDate('2018-11-09 00:00:00');
-			$this->assertTrue(strstr($resEvent->getRaw(true), 'DSTART') && strstr($resEvent->getRaw(), '20181109'));
+			$this->assertTrue(strstr($resEvent->getRaw(true), 'DSTART') && strstr($resEvent->getRaw(true), '20181109'));
 
 			$this->assertNull($resEvent->endDate());
 			$resEvent->endDate('2018-11-10 00:00:00');
-			$this->assertTrue(strstr($resEvent->getRaw(true), 'DEND') && strstr($resEvent->getRaw(), '20181110'));
+			$this->assertTrue(strstr($resEvent->getRaw(true), 'DEND') && strstr($resEvent->getRaw(true), '20181110'));
 
 			$this->assertNull($resEvent->allDay());
 			$resEvent->allDay('2018-11-09 00:00:00');
@@ -28,21 +28,21 @@
 
 			$this->assertNull($resEvent->title());
 			$resEvent->title('test-title');
-			$this->assertTrue(strstr($resEvent->getRaw(true), 'SUMMARY') && strstr($resEvent->getRaw(), 'test-title'));
+			$this->assertTrue(strstr($resEvent->getRaw(true), 'SUMMARY') && strstr($resEvent->getRaw(true), 'test-title'));
 
 			$this->assertNull($resEvent->description());
 			$resEvent->description('test-desc');
-			$this->assertTrue(strstr($resEvent->getRaw(true), 'DESCRIPTION') && strstr($resEvent->getRaw(), 'test-desc'));
+			$this->assertTrue(strstr($resEvent->getRaw(true), 'DESCRIPTION') && strstr($resEvent->getRaw(true), 'test-desc'));
 
 			$this->assertNull($resEvent->location());
 			$resEvent->location('test-location');
-			$this->assertTrue(strstr($resEvent->getRaw(true), 'LOCATION') && strstr($resEvent->getRaw(), 'test-location'));
+			$this->assertTrue(strstr($resEvent->getRaw(true), 'LOCATION') && strstr($resEvent->getRaw(true), 'test-location'));
 
 			$this->assertNull($resEvent->url());
 			$resEvent->url('https://twistphp.com');
-			$this->assertTrue(strstr($resEvent->getRaw(true), 'URL') && strstr($resEvent->getRaw(), 'https://twistphp.com'));
+			$this->assertTrue(strstr($resEvent->getRaw(true), 'URL') && strstr($resEvent->getRaw(true), 'https://twistphp.com'));
 
-			//Now check teh output without bypassing validation
+			//Now check the output without bypassing validation
 			$this->assertTrue(strstr($resEvent->getRaw(), 'BEGIN:VEVENT') !== false);
 		}
 	}
