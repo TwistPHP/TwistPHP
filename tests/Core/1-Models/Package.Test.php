d914eeeeed1c5d7348458f04809d7120247cdcd9
danrwalker <danrwalker1@gmail.com>
2019-03-07 11:22:30 +0000

    Test packge install and uninstall process

---
+++ tests/Core/1-Models/Package.Test.php
@@ -0,0 +1,37 @@
+<?php
+
+	use PHPUnit\Framework\TestCase;
+
+	require_once dirname(__FILE__).'/../../phpunit-support.php';
+
+	class Package extends PHPUnitSupport{
+
+		public function testList(){
+
+			$arrPackages = \Twist::framework()->package()->getAll();
+			$this->assertArrayHasKey('manager',$arrPackages);
+			$this->assertArrayHasKey('notifications',$arrPackages);
+		}
+
+		public function testInstall(){
+
+			$this->assertFalse(\Twist::framework()->package()->isInstalled('notifications'));
+
+			$this->assertTrue(\Twist::framework()->package()->installer('notifications'));
+
+			$this->assertTrue(\Twist::framework()->package()->isInstalled('notifications'));
+		}
+
+		public function testInformation(){
+
+			$this->assertArrayHasKey('slug',\Twist::framework()->package()->information('notifications'));
+		}
+
+		public function testUninstall(){
+
+			$this->assertTrue(\Twist::framework()->package()->uninstaller('notifications'));
+
+			$this->assertFalse(\Twist::framework()->package()->isInstalled('notifications'));
+		}
+
+	}
