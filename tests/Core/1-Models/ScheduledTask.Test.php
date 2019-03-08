9e788cff5237daadddb1e32bbba9485f44b42bc6
Dan Walker <danrwalker1@gmail.com>
2019-03-08 15:34:44 +0000

    Added a new task that can be run as test

--- tests/Core/1-Models/ScheduledTask.Test.php
+++ tests/Core/1-Models/ScheduledTask.Test.php
@@ -8,16 +8,36 @@
 
 		public function testRun(){
 
+			//Enable a task
+			$intTaskID = Twist\Core\Models\ScheduledTasks::createTask(
+				'Travis Test',
+				1,
+				'twist/Core/Crons/ProtectFirewall.cron.php',
+				0,
+				'',
+				true,
+				'travis'
+			);
+
+			//Run the Cron
 			$arrPulse = Twist\Core\Models\ScheduledTasks::pulseInfo();
 			$this->assertFalse($arrPulse['active']);
 
+			ob_start();
+
 			Twist\Core\Models\ScheduledTasks::processor();
 			sleep(1);
 			Twist\Core\Models\ScheduledTasks::processor();
 			sleep(1);
 			Twist\Core\Models\ScheduledTasks::processor();
 
+			ob_end_clean();
+
 			$arrPulse = Twist\Core\Models\ScheduledTasks::pulseInfo();
 			$this->assertTrue($arrPulse['active']);
+
+			$arrTask = Twist\Core\Models\ScheduledTasks::get($intTaskID);
+			$this->assertStringContainsString($arrTask['status'],'finished');
+
 		}
 	}
