eca04f24da9f7d2138e8533fb2b8255142e659bc
Dan Walker <danrwalker1@gmail.com>
2019-03-08 23:45:27 +0000

    Simplified the PHPUnit Support

--- tests/phpunit-support.php
+++ tests/phpunit-support.php
@@ -1,46 +1,29 @@
 <?php
 
 	use PHPUnit\Framework\TestCase;
-
-	class TestCaseTest extends TestCase{
-		public function testFunctions(){
-			$this->assertTrue(true);
-		}
-	}
-
-	if(method_exists(new TestCaseTest(),'assertStringContainsString')){
+	
+	/**
+	 * Class PHPUnitSupport
+	 * Add backwards compatible support for testing older versions of PHP
+	 */
+	class PHPUnitSupport extends TestCase{
 
 		/**
-		 * Class PHPUnitSupport
-		 * Add forward compatible support for testing newer versions of PHP
+		 * Add support for a new function as assertContains was deprecated in old PHPUnit
+		 * @param $strValue1
+		 * @param $strValue2
+		 * @return mixed
 		 */
-		class PHPUnitSupport extends TestCase{
+		public function assertStringContainsString($strValue1,$strValue2){
 
-			public function testSupport(){
-				$this->assertTrue(true);
-			}
-		}
-
-	}else{
-
-		/**
-		 * Class PHPUnitSupport
-		 * Add backwards compatible support for testing older versions of PHP
-		 */
-		class PHPUnitSupport extends TestCase{
-
-			/**
-			 * Add support for a new function as assertContains was deprecated in old PHPUnit
-			 * @param $strValue1
-			 * @param $strValue2
-			 * @return mixed
-			 */
-			public function assertStringContainsString($strValue1,$strValue2){
+			if(method_exists(parent,'assertStringContainsString')){
+				return parent::assertStringContainsString($strValue1,$strValue2);
+			}else{
 				return $this->assertContains($strValue1,$strValue2);
 			}
+		}
 
-			public function testSupport(){
-				$this->assertTrue(true);
-			}
+		public function testSupport(){
+			$this->assertTrue(true);
 		}
 	}
